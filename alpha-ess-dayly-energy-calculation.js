0ffd32ed98e1ba3a923dc1f53703230bfb0f216a
Gaspode69 <45949835+Gaspode69@users.noreply.github.com>
2024-09-27 10:58:14 +0200

    Update alpha-ess-dayly-energy-calculation.js

--- alpha-ess-dayly-energy-calculation.js
+++ alpha-ess-dayly-energy-calculation.js
@@ -1,10 +1,14 @@
 //-------------------------------------------------------------------------------------------------------------------
 // ioBroker JavaScript:
-// Berechne den bisherigen Tagesverbrauch aller Verbraucher im Haushalt.
+// Berechne aktuellen Tagesverbrauch in kWh, Autarkiegrad, Eigenverbrauch und generierte Einnahmen
+// von Alpha-ESS Systemen auf Basis von Modbuswerten.
+//
 // Nachts um 0:00 Uhr werden die Tageswerte wieder auf 0 zurück gesetzt (kann deaktiviert werden, s.u.)
 // 
 // Die State Namen entsprechen den Modbus Definitionen von hier:
 // https://github.com/ioBroker/modbus-templates/blob/main/PV-Wechselrichter/Alpha-ESS/holding_register.txt
+// Damit das Script unverändert funktioniert, muss der Modbus Adapter Instanz 0 sein und der Haken bei
+// "Adresse nicht in ID aufnehmen" muss gesetzt sein.
 //
 // 23.02.2024  V1.0.0 (Gaspode) Erste Version
 // 25.02.2024  V1.0.1 (Gaspode) Ignoriere Tagessumme, wenn rückläufig
@@ -14,15 +18,25 @@
 //                              Diverse interne Anpassungen
 //                              ACHTUNG: Der State Name für den GEsamtverbrauch wurde umbenannt von 
 //                                       HoseLoad zu Consumption_House" 
+// 24.09.2024  V2.1.0 (Gaspode) Zusätzliche Tageswerte für Eigenverbrauch, Autarkiegrad und Einnahmen
+//                              Danke an reini72 von Storion4you.de 
 //-------------------------------------------------------------------------------------------------------------------
+
 
 // Setze auf false, wenn um Mitternacht nicht zurückgesetzt werden soll, sondern weitergezählt werden soll
 const resetAtMidnight = true;
 
+// Preise für Einnahmenberechnung
+const incomeUnit = '€';
+const priceBuy   = 0.4;
+const priceSell  = 0.0803;
+
 // Root Ordner im ioBroker Objektbaum:
-const modbusRoot   = 'modbus.0.holdingRegisters.';
-const midnightRoot = '0_userdata.0.PV.midnight.';
-const resultRoot   = '0_userdata.0.PV.';
+const modbusRoot      = 'modbus.0.holdingRegisters.';
+const resultRootx      = '0_userdata.0.PV.';
+const midnightRoot    = resultRootx + 'midnight.';
+const resultRootToday = resultRootx + 'today.';
+const resultRootTotal = resultRootx + 'total.';
 
 // Namen der Modbus States:
 const gridConsumptionStateName  = '_Total_energy_consume_from_grid_(Grid_Meter)';
@@ -31,31 +45,46 @@ const batteryChargeStateName    = '_Battery_charge_energy';
 const pvGenerationStateName     = '_Total_PV_Energy';
 const gridFeedInStateName       = '_Total_energyfeed_to_grid_(Grid_Meter)';
 
-// Namen der Ergebins States:
-const houseConsumptionStateName    = 'Consumption_House';
-const directPVConsumptionStateName = 'Consumption_DirectPV';
+// Namen der Ergebnis States:
+const houseConsumptionStateName     = 'Consumption_House'; // Hausverbrauch heute [kWh]
+const directPVConsumptionStateName  = 'Consumption_DirectPV'; // PV Direktverbrauch heute [kWh]
+const incomeTodayStateName          = 'Income'; // Einnahmen heute [€]
+const selfSufficiencyTodayStateName = 'Self_sufficiency'; // Autarkiegrad heute [%]
+const selfConsumptionTodayStateName = 'Self_consumption'; // Anteil_Eigenverbrauch heute [%]
+const incomeTotalStateName          = 'Income'; // Einnahmen gesamt [€]
+const selfSufficiencyTotalStateName = 'Self_sufficiency'; // Autarkiegrad gesamt [%]
+const selfConsumptionTotalStateName = 'Self_consumption'; // Anteil_Eigenverbrauch gesamt [%]
 
-
+// Variablen für aktuelle Werte
 let gridConsumption  = 0;
 let batteryDischarge = 0;
 let batteryCharge    = 0;
 let pvGeneration     = 0;
 let gridFeedIn       = 0;
 
-let gridConsumptionMN   = 0;
-let batteryDischargeMN  = 0;
-let batteryChargeMN     = 0;
-let pvGenerationMN      = 0;
-let gridFeedInMN        = 0;
-let houseConsumption    = 0;
-let directPVConsumption = 0;
+// Variablen für Mitternachtswerte
+let gridConsumptionMN    = 0;
+let batteryDischargeMN   = 0;
+let batteryChargeMN      = 0;
+let pvGenerationMN       = 0;
+let gridFeedInMN         = 0;
+
+// Variablen für berechnete Werte
+let houseConsumption     = 0;
+let directPVConsumption  = 0;
+let incomeToday          = 0;
+let selfSufficiencyToday = 0;
+let selfConsumptionToday = 0;
+let incomeTotal          = 0;
+let selfSufficiencyTotal = 0;
+let selfConsumptionTotal = 0;
 
 function updateHouseConsumptionState()
 {
     const houseConsumptionNew = gridConsumption + batteryDischarge + pvGeneration - batteryCharge - gridFeedIn;
     if (houseConsumptionNew > houseConsumption) {
         houseConsumption = houseConsumptionNew;
-        setState (resultRoot + houseConsumptionStateName, houseConsumption);
+        setState (resultRootToday + houseConsumptionStateName, houseConsumption);
     }
 }
 
@@ -64,8 +93,40 @@ function updateDirectPVConsumptionState()
     const directPVConsumptionNew = pvGeneration - batteryCharge - gridFeedIn;
     if (directPVConsumptionNew > directPVConsumption) {
         directPVConsumption = directPVConsumptionNew;
-        setState (resultRoot + directPVConsumptionStateName, directPVConsumption);
+        setState (resultRootToday + directPVConsumptionStateName, directPVConsumption);
     }
+}
+
+function updateIncomeStates()
+{
+    incomeToday = roundTo (((pvGeneration - gridFeedIn) * priceBuy) + (gridFeedIn * priceSell), 2);
+    setState (resultRootToday + incomeTodayStateName, incomeToday);
+
+    incomeTotal = roundTo ((((pvGeneration + pvGenerationMN) - (gridFeedIn + gridFeedInMN)) * priceBuy) + ((gridFeedIn + gridFeedInMN) * priceSell), 2);
+    setState (resultRootTotal + incomeTotalStateName, incomeTotal);
+}
+
+function updateSelfSufficiencyStates()
+{
+    selfSufficiencyToday = roundTo (((pvGeneration - gridFeedIn) * 100) / (gridConsumption + pvGeneration - gridFeedIn), 2);
+    setState (resultRootToday + selfSufficiencyTodayStateName, selfSufficiencyToday);
+
+    selfSufficiencyTotal = roundTo ((((pvGeneration + pvGenerationMN) - (gridFeedIn + gridFeedInMN)) * 100) / (gridConsumption + gridConsumptionMN + pvGeneration + pvGenerationMN - (gridFeedIn + gridFeedIn)), 2);
+    setState (resultRootTotal + selfSufficiencyTotalStateName, selfSufficiencyTotal);
+}
+
+function updateSelfConsumptionStates()
+{
+    selfConsumptionToday = roundTo (((pvGeneration - gridFeedIn) * 100) / pvGeneration, 2);
+    setState (resultRootToday + selfConsumptionTodayStateName, selfConsumptionToday);
+
+    selfConsumptionTotal = roundTo ((((pvGeneration + pvGenerationMN) - (gridFeedIn + gridFeedInMN)) * 100) / (pvGeneration + pvGenerationMN), 2);
+    setState (resultRootTotal + selfConsumptionTotalStateName, selfConsumptionTotal);
+}
+
+function roundTo(num, precision) {
+  const factor = Math.pow(10, precision)
+  return Math.round(num * factor) / factor
 }
 
 async function copyMidnightState (stateName)
@@ -73,9 +134,9 @@ async function copyMidnightState (stateName)
     setState (midnightRoot + stateName, getState (modbusRoot + stateName).val);
 }
 
-async function setResultState (stateName, value)
+async function setResultState (resRoot, stateName, value)
 {
-    setState (resultRoot + stateName, value);
+    setState (resRoot + stateName, value);
 }
 
 async function copyMidnightStates()
@@ -89,23 +150,29 @@ async function copyMidnightStates()
 
 async function resetResultStates()
 {
-    await setResultState (gridConsumptionStateName, 0);
-    await setResultState (batteryDischargeStateName, 0);
-    await setResultState (batteryChargeStateName, 0);
-    await setResultState (pvGenerationStateName, 0);
-    await setResultState (gridFeedInStateName, 0);
+    await setResultState (resultRootToday, gridConsumptionStateName, 0);
+    await setResultState (resultRootToday, batteryDischargeStateName, 0);
+    await setResultState (resultRootToday, batteryChargeStateName, 0);
+    await setResultState (resultRootToday, pvGenerationStateName, 0);
+    await setResultState (resultRootToday, gridFeedInStateName, 0);
+    await setResultState (resultRootToday, incomeTodayStateName, 0);
+    await setResultState (resultRootToday, selfSufficiencyTodayStateName, 0);
+    await setResultState (resultRootToday, selfConsumptionTodayStateName, 0);
+    await setResultState (resultRootTotal, incomeTotalStateName, 0);
+    await setResultState (resultRootTotal, selfSufficiencyTotalStateName, 0);
+    await setResultState (resultRootTotal, selfConsumptionTotalStateName, 0);
 }
 
-async function initState(stateName, createMNState)
+async function initState(resRoot, stateName, createMNState, unit)
 {
     if (createMNState && !existsState (midnightRoot + stateName)) {
-        await createStateAsync (midnightRoot + stateName, 0, {unit: 'kWh', type: 'number'});
+        await createStateAsync (midnightRoot + stateName, 0, {unit: unit, type: 'number'});
         await copyMidnightState (stateName);
     }
 
-    if (!existsState (resultRoot + stateName)) {
-        await createStateAsync (resultRoot + stateName, 0, {unit: 'kWh', type: 'number'});
-        await setResultState (stateName, 0);
+    if (!existsState (resRoot + stateName)) {
+        await createStateAsync (resRoot + stateName, 0, {unit: unit, type: 'number'});
+        await setResultState (resRoot, stateName, 0);
     }
 }
 
@@ -117,26 +184,42 @@ async function initValues()
     pvGenerationMN     = getState (midnightRoot + pvGenerationStateName).val;
     gridFeedInMN       = getState (midnightRoot + gridFeedInStateName).val;
 
-    gridConsumption  = getState (resultRoot + gridConsumptionStateName).val;
-    batteryDischarge = getState (resultRoot + batteryDischargeStateName).val;
-    batteryCharge    = getState (resultRoot + batteryChargeStateName).val;
-    pvGeneration     = getState (resultRoot + pvGenerationStateName).val;
-    gridFeedIn       = getState (resultRoot + gridFeedInStateName).val;
+    gridConsumption  = getState (resultRootToday + gridConsumptionStateName).val;
+    batteryDischarge = getState (resultRootToday + batteryDischargeStateName).val;
+    batteryCharge    = getState (resultRootToday + batteryChargeStateName).val;
+    pvGeneration     = getState (resultRootToday + pvGenerationStateName).val;
+    gridFeedIn       = getState (resultRootToday + gridFeedInStateName).val;
 
-    houseConsumption    = getState (resultRoot + houseConsumptionStateName).val;
-    directPVConsumption = getState (resultRoot + directPVConsumptionStateName).val;
+    houseConsumption    = getState (resultRootToday + houseConsumptionStateName).val;
+    directPVConsumption = getState (resultRootToday + directPVConsumptionStateName).val;
+
+    incomeToday          = getState (resultRootToday + incomeTodayStateName).val;
+    selfSufficiencyToday = getState (resultRootToday + selfSufficiencyTodayStateName).val;
+    selfConsumptionToday = getState (resultRootToday + selfConsumptionTodayStateName).val;
+
+    incomeTotal          = getState (resultRootTotal + incomeTotalStateName).val;
+    selfSufficiencyTotal = getState (resultRootTotal + selfSufficiencyTotalStateName).val;
+    selfConsumptionTotal = getState (resultRootTotal + selfConsumptionTotalStateName).val;
 }
 
 async function init()
 {
-    await initState (gridConsumptionStateName, true);
-    await initState (batteryDischargeStateName, true);
-    await initState (batteryChargeStateName, true);
-    await initState (pvGenerationStateName, true);
-    await initState (gridFeedInStateName, true);
+    await initState (resultRootToday, gridConsumptionStateName, true, 'kWh');
+    await initState (resultRootToday, batteryDischargeStateName, true, 'kWh');
+    await initState (resultRootToday, batteryChargeStateName, true, 'kWh');
+    await initState (resultRootToday, pvGenerationStateName, true, 'kWh');
+    await initState (resultRootToday, gridFeedInStateName, true, 'kWh');
 
-    await initState (houseConsumptionStateName, false);
-    await initState (directPVConsumptionStateName, false);
+    await initState (resultRootToday, houseConsumptionStateName, false, 'kWh');
+    await initState (resultRootToday, directPVConsumptionStateName, false, 'kWh');
+
+    await initState (resultRootToday, incomeTodayStateName, false, incomeUnit);
+    await initState (resultRootToday, selfSufficiencyTodayStateName, false, '%');
+    await initState (resultRootToday, selfConsumptionTodayStateName, false, '%');
+
+    await initState (resultRootTotal, incomeTotalStateName, false, incomeUnit);
+    await initState (resultRootTotal, selfSufficiencyTotalStateName, false, '%');
+    await initState (resultRootTotal, selfConsumptionTotalStateName, false, '%');
 
     await initValues();
 
@@ -148,35 +231,45 @@ init();
 
 on({ id: modbusRoot + gridConsumptionStateName, change: 'ne' }, (obj) => {
     gridConsumption = obj.state.val - gridConsumptionMN;
-    setState(resultRoot + gridConsumptionStateName, gridConsumption);
+    setState(resultRootToday + gridConsumptionStateName, gridConsumption);
     updateHouseConsumptionState();
+    updateIncomeStates();
+    updateSelfSufficiencyStates();
+    updateSelfConsumptionStates();
 });
 
 on({ id: modbusRoot + batteryDischargeStateName, change: 'ne' }, (obj) => {
     batteryDischarge = obj.state.val - batteryDischargeMN;
-    setState(resultRoot + batteryDischargeStateName, batteryDischarge);
+    setState(resultRootToday + batteryDischargeStateName, batteryDischarge);
     updateHouseConsumptionState();
 });
 
 on({ id: modbusRoot + batteryChargeStateName, change: 'ne' }, (obj) => {
+
     batteryCharge = obj.state.val - batteryChargeMN;
-    setState(resultRoot + batteryChargeStateName, batteryCharge);
+    setState(resultRootToday + batteryChargeStateName, batteryCharge);
     updateHouseConsumptionState();
     updateDirectPVConsumptionState();
 });
 
 on({ id: modbusRoot + pvGenerationStateName, change: 'ne' }, (obj) => {
     pvGeneration = obj.state.val - pvGenerationMN;
-    setState(resultRoot + pvGenerationStateName, pvGeneration);
+    setState(resultRootToday + pvGenerationStateName, pvGeneration);
     updateHouseConsumptionState();
     updateDirectPVConsumptionState();
+    updateIncomeStates();
+    updateSelfSufficiencyStates();
+    updateSelfConsumptionStates();
 });
 
 on({ id: modbusRoot + gridFeedInStateName, change: 'ne' }, (obj) => {
     gridFeedIn = obj.state.val - gridFeedInMN;
-    setState(resultRoot + gridFeedInStateName, gridFeedIn);
+    setState(resultRootToday + gridFeedInStateName, gridFeedIn);
     updateHouseConsumptionState();
     updateDirectPVConsumptionState();
+    updateIncomeStates();
+    updateSelfSufficiencyStates();
+    updateSelfConsumptionStates();
 });
 
 // 5 Sekunden nach Mitternacht umspeichern der Modbus PV Energiewerte
@@ -185,9 +278,10 @@ if (resetAtMidnight) {
         await copyMidnightStates();
         await resetResultStates();
         await initValues();
-        houseConsumption = 0;
-        directPVConsumption = 0;
         updateHouseConsumptionState();
         updateDirectPVConsumptionState();        
+        updateIncomeStates();
+        updateSelfSufficiencyStates();
+        updateSelfConsumptionStates();
     });
 }
